c282ab04400208b8733f692c5e3e0d076a91649d
Yandrapu <yandrapu.atchyutha@capgemini.com>
2023-09-25 16:13:59 +0530

    Vasu

--- src/main/java/com/example/ServiceBus/controller/ServiceBusController.java
+++ src/main/java/com/example/ServiceBus/controller/ServiceBusController.java
@@ -21,7 +21,7 @@ import java.util.stream.Collectors;
 @RequestMapping("/api")
 public class ServiceBusController {
 
-    String str = "Sindhu";
+    String str = "Sindhujha";
 
     private static final String[] Summaries = {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
