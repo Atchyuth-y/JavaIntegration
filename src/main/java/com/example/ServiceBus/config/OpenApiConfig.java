b544041cec25043476cf8179f831d7cc975c4a00
Yandrapu <yandrapu.atchyutha@capgemini.com>
2023-10-03 11:40:25 +0530

    added payload

---
+++ src/main/java/com/example/ServiceBus/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+//package com.example.ServiceBus.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springdoc.core.GroupedOpenApi;
+//import org.springdoc.core.SwaggerUiConfigParameters;
+//
+//@Configuration
+//public class OpenApiConfig {
+//
+//    @Bean
+//    public GroupedOpenApi customOpenApi() {
+//        return GroupedOpenApi.builder()
+//                .group("custom")
+//                .packagesToScan("com.example.ServiceBus.config")
+//                .build();
+//    }
+//}
+//
